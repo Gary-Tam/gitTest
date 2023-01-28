57d2beef1c5bb2af6f629a0079f660812964398f
GaryTam <2318243133@qq.com>
2023-01-28 18:06:39 +0800

    idea first merge commit

---
+++ src/main/java/com/example/demo/test/MergeTest.java
@@ -0,0 +1,16 @@
+package com.example.demo.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author:Gary
+ * @Description:
+ * @params:$
+ * @return: $
+ * @Date: 2023/01/28/下午6:06
+ */
+public class MergeTest {
+    public static void main(String[] args) {
+        System.out.println("this is a merge test");
+    }
+}
