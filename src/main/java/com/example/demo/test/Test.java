e28e2e59f894c8f80162e4f1f26c2ed30c8670ed
GaryTam <2318243133@qq.com>
2023-01-28 17:45:43 +0800

    idea first commit

---
+++ src/main/java/com/example/demo/test/Test.java
@@ -0,0 +1,16 @@
+package com.example.demo.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author:Gary
+ * @Description:
+ * @params:$
+ * @return: $
+ * @Date: 2023/01/28/下午5:43
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("git version1");
+    }
+}
